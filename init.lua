884bdbbf8f4e3d0d2b1cee69d2ce842b02fd1aca
Merveille van Eck <merveillevaneck@gmail.com>
2025-07-01 21:47:11 +0200

    disabled telescope completely

--- init.lua
+++ init.lua
@@ -448,104 +448,104 @@ require('lazy').setup({
   --
   -- Use the `dependencies` key to specify the dependencies of a particular plugin
 
-  { -- Fuzzy Finder (files, lsp, etc)
-    'nvim-telescope/telescope.nvim',
-    event = 'VimEnter',
-    dependencies = {
-      'nvim-lua/plenary.nvim',
-      { -- If encountering errors, see telescope-fzf-native README for installation instructions
-        'nvim-telescope/telescope-fzf-native.nvim',
+  --{ -- Fuzzy Finder (files, lsp, etc)
+  --  'nvim-telescope/telescope.nvim',
+  --  event = 'VimEnter',
+  --  dependencies = {
+  --    'nvim-lua/plenary.nvim',
+  --    { -- If encountering errors, see telescope-fzf-native README for installation instructions
+  --      'nvim-telescope/telescope-fzf-native.nvim',
 
-        -- `build` is used to run some command when the plugin is installed/updated.
-        -- This is only run then, not every time Neovim starts up.
-        build = 'make',
+  --      -- `build` is used to run some command when the plugin is installed/updated.
+  --      -- This is only run then, not every time Neovim starts up.
+  --      build = 'make',
 
-        -- `cond` is a condition used to determine whether this plugin should be
-        -- installed and loaded.
-        cond = function()
-          return vim.fn.executable 'make' == 1
-        end,
-      },
-      { 'nvim-telescope/telescope-ui-select.nvim' },
+  --      -- `cond` is a condition used to determine whether this plugin should be
+  --      -- installed and loaded.
+  --      cond = function()
+  --        return vim.fn.executable 'make' == 1
+  --      end,
+  --    },
+  --    { 'nvim-telescope/telescope-ui-select.nvim' },
 
-      -- Useful for getting pretty icons, but requires a Nerd Font.
-      { 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font },
-    },
-    config = function()
-      require('telescope').setup {
-        pickers = {
-          find_files = {
-            hidden = true,
-          },
-        },
-				layout_config = {
-					vertical = { width = 0.5 }
-				},
-        extensions = {
-          ['ui-select'] = {
-            require('telescope.themes').get_dropdown(),
-          },
-        },
-      }
+  --    -- Useful for getting pretty icons, but requires a Nerd Font.
+  --    { 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font },
+  --  },
+  --  config = function()
+  --    require('telescope').setup {
+  --      pickers = {
+  --        find_files = {
+  --          hidden = true,
+  --        },
+  --      },
+	--			layout_config = {
+	--				vertical = { width = 0.5 }
+	--			},
+  --      extensions = {
+  --        ['ui-select'] = {
+  --          require('telescope.themes').get_dropdown(),
+  --        },
+  --      },
+  --    }
 
-      -- Enable Telescope extensions if they are installed
-      pcall(require('telescope').load_extension, 'fzf')
-      pcall(require('telescope').load_extension, 'ui-select')
+  --    -- Enable Telescope extensions if they are installed
+  --    pcall(require('telescope').load_extension, 'fzf')
+  --    pcall(require('telescope').load_extension, 'ui-select')
 
-      -- See `:help telescope.builtin`
-      local builtin = require 'telescope.builtin'
-      vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
-      vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
-      vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
-      vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
-      vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
-      --vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
-      vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
-      vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
-      vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
+  --    -- See `:help telescope.builtin`
+  --    local builtin = require 'telescope.builtin'
+  --    vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
+  --    vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
+  --    vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
+  --    vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
+  --    vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
+  --    --vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
+  --    vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
+  --    vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
+  --    vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
 
-      --local findfiles = function()
-      --  builtin.find_files { hidden = true, no_ignore = true }
-      --end
-      --vim.keymap.set('n', '<c-p>', findfiles, { desc = '[S]earch [F]iles' })
+  --    --local findfiles = function()
+  --    --  builtin.find_files { hidden = true, no_ignore = true }
+  --    --end
+  --    --vim.keymap.set('n', '<c-p>', findfiles, { desc = '[S]earch [F]iles' })
 
-      local vsplitscope = function()
-        vim.cmd 'vs'
-      end
+  --    local vsplitscope = function()
+  --      vim.cmd 'vs'
+  --    end
 
-      vim.keymap.set('n', '<C-.>', vsplitscope)
-      vim.keymap.set('n', '<C-,>', '<Esc><Cmd>ConjureLogVSplit<CR>')
+  --    vim.keymap.set('n', '<C-.>', vsplitscope)
+  --    vim.keymap.set('n', '<C-,>', '<Esc><Cmd>ConjureLogVSplit<CR>')
 
-      -- vim.keymap.set('n', '<C-b>', '<Cmd>Neotree toggle<CR>')
+  --    -- vim.keymap.set('n', '<C-b>', '<Cmd>Neotree toggle<CR>')
 
-      -- Slightly advanced example of overriding default behavior and theme
-      vim.keymap.set('n', '<leader>/', function()
-        -- You can pass additional configuration to Telescope to change the theme, layout, etc.
-        builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
-          winblend = 10,
-          previewer = false,
-        })
-      end, { desc = '[/] Fuzzily search in current buffer' })
+  --    -- Slightly advanced example of overriding default behavior and theme
+  --    vim.keymap.set('n', '<leader>/', function()
+  --      -- You can pass additional configuration to Telescope to change the theme, layout, etc.
+  --      builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
+  --        winblend = 10,
+  --        previewer = false,
+  --      })
+  --    end, { desc = '[/] Fuzzily search in current buffer' })
 
-			--vim.keymap.set('n', '<leader>sg', function()
-			--	local fzf = require('fzf-lua')
-			--end)
+	--		--vim.keymap.set('n', '<leader>sg', function()
+	--		--	local fzf = require('fzf-lua')
+	--		--end)
 
-      -- It's also possible to pass additional configuration options.
-      --  See `:help telescope.builtin.live_grep()` for information about particular keys
-      vim.keymap.set('n', '<leader>s/', function()
-        builtin.live_grep {
-          grep_open_files = true,
-          prompt_title = 'Live Grep in Open Files',
-        }
-      end, { desc = '[S]earch [/] in Open Files' })
+  --    -- It's also possible to pass additional configuration options.
+  --    --  See `:help telescope.builtin.live_grep()` for information about particular keys
+  --    vim.keymap.set('n', '<leader>s/', function()
+  --      builtin.live_grep {
+  --        grep_open_files = true,
+  --        prompt_title = 'Live Grep in Open Files',
+  --      }
+  --    end, { desc = '[S]earch [/] in Open Files' })
 
-      -- Shortcut for searching your Neovim configuration files
-      vim.keymap.set('n', '<leader>sn', function()
-        builtin.find_files { cwd = vim.fn.stdpath 'config' }
-      end, { desc = '[S]earch [N]eovim files' })
-    end,
-  },
+  --    -- Shortcut for searching your Neovim configuration files
+  --    vim.keymap.set('n', '<leader>sn', function()
+  --      builtin.find_files { cwd = vim.fn.stdpath 'config' }
+  --    end, { desc = '[S]earch [N]eovim files' })
+  --  end,
+  --},
 
   -- LSP Plugins
   {
@@ -629,33 +629,33 @@ require('lazy').setup({
           map('gra', vim.lsp.buf.code_action, '[G]oto Code [A]ction', { 'n', 'x' })
 
           -- Find references for the word under your cursor.
-          map('grr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
+          --map('grr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
 
           -- Jump to the implementation of the word under your cursor.
           --  Useful when your language has ways of declaring types without an actual implementation.
-          map('gri', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
+          --map('gri', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
 
           -- Jump to the definition of the word under your cursor.
           --  This is where a variable was first declared, or where a function is defined, etc.
           --  To jump back, press <C-t>.
-          map('grd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
+          --map('grd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
 
           -- WARN: This is not Goto Definition, this is Goto Declaration.
           --  For example, in C this would take you to the header.
-          map('grD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+          --map('grD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
 
           -- Fuzzy find all the symbols in your current document.
           --  Symbols are things like variables, functions, types, etc.
-          map('gO', require('telescope.builtin').lsp_document_symbols, 'Open Document Symbols')
+          --map('gO', require('telescope.builtin').lsp_document_symbols, 'Open Document Symbols')
 
           -- Fuzzy find all the symbols in your current workspace.
           --  Similar to document symbols, except searches over your entire project.
-          map('gW', require('telescope.builtin').lsp_dynamic_workspace_symbols, 'Open Workspace Symbols')
+          --map('gW', require('telescope.builtin').lsp_dynamic_workspace_symbols, 'Open Workspace Symbols')
 
           -- Jump to the type of the word under your cursor.
           --  Useful when you're not sure what type a variable is and you want to see
           --  the definition of its *type*, not where it was *defined*.
-          map('grt', require('telescope.builtin').lsp_type_definitions, '[G]oto [T]ype Definition')
+          --map('grt', require('telescope.builtin').lsp_type_definitions, '[G]oto [T]ype Definition')
 
           -- This function resolves a difference between neovim nightly (version 0.11) and stable (version 0.10)
           ---@param client vim.lsp.Client
